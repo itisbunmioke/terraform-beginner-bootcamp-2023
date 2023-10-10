a7ecc03f9a5e32a8eefed1261e99f454d8fab341
Bunmi Oke <itisbunmioke@gmail.com>
2023-10-10 03:15:31 -0600

    45 terratowns provider (#47)
    * #45 wip setting up the skeleton for creating a customer terraform provider
    
    * #45 implement terraform provider block in terraform

--- modules/terrahouse_aws/main.tf
+++ modules/terrahouse_aws/main.tf
@@ -7,6 +7,6 @@ terraform {
   }
 }
 
-# https://developer.hashicorp.com/terraform/language/data-sources
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/caller_identity
 
 data "aws_caller_identity" "current" {}
